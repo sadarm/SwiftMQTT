9045fcc5d003963192922535281ceac972b41e77
kisu-park <kisu.park@navercorp.com>
2023-01-07 13:44:19 +0900

    swift package init

---
+++ Sources/SwiftMQTT/SwiftMQTT.swift
@@ -0,0 +1,6 @@
+public struct SwiftMQTT {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftMQTTTests/SwiftMQTTTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftMQTT
+
+final class SwiftMQTTTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftMQTT().text, "Hello, World!")
+    }
+}
