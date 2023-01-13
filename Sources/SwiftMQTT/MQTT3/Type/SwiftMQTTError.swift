b2c56ceb9405993f08a71ed40d4eda5898f2ef9a
kisu-park <kisu.park@navercorp.com>
2023-01-14 01:18:38 +0900

    add errors

--- Sources/SwiftMQTT/MQTT3/Type/SwiftMQTTError.swift
+++ Sources/SwiftMQTT/MQTT3/Type/SwiftMQTTError.swift
@@ -8,10 +8,16 @@
 import Foundation
 
 public enum SwiftMQTTError: Error {
+    case unknown
     case notEnoughData
     case corruptData
     case unexpectedType
     case typeMissmatch
     case incorrentBytes
+    case unacceptableProtocolVersion
+    case identifierRejected
+    case serverUnavailable
+    case badUserNameOrPassword
+    case notAuthorized
 }
 
