5ab0b5d7d089a5d66edce82ff2e74732a3f81d00
kisu-park <kisu.park@navercorp.com>
2023-01-16 09:45:07 +0900

    add unit test

--- Tests/SwiftMQTTTests/SwiftMQTTTests.swift
+++ Tests/SwiftMQTTTests/SwiftMQTTTests.swift
@@ -2,7 +2,24 @@ import XCTest
 @testable import SwiftMQTT
 
 final class SwiftMQTTTests: XCTestCase {
-    func testExample() throws {
+    func testExample() async throws {
+        let parameters = MQTT3Connection.Parameters(host: "localhost",
+                                                    port: 1883,
+                                                    clientID: "UnitTest",
+                                                    cleanSession: false,
+                                                    keepAlive: 5)
+        let connection = MQTT3Connection(using: parameters)
+        try await connection.start()
+        try await connection.subscribeTopics([("/stat", .qos2)])
         
+        let expection = expectation(description: "waiting")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 60.0, execute: {
+            expection.fulfill()
+        })
+        
+        await waitForExpectations(timeout: 100.0)
+        
+        _ = connection
     }
 }
